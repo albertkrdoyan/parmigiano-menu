bea4c7e9f961ea6f90e0a8a47d574d13283969ae
Albert Krdoyan <albert.krdoyan1@gmail.com>
2026-03-25 00:54:04 +0400

    25.03.2026 00:54

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:parmigiano_menu/Utils/constants.dart';
 import 'Pages/MenuPage.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const Application());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: Application()),);
 }
 
 class Application extends StatelessWidget {
