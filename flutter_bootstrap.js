dc6240f24bc4fb0b38165d4b5a451b2d39228212
Albert Krdoyan <albert.krdoyan1@gmail.com>
2026-04-01 16:35:23 +0400

    Built web
    Branch: main
    Commit: 7854ca938b8fb2d22fc7dbea24949be2487702c7 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3612041256" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4288873602" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
